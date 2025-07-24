c54a7c9df60fae878e7147c066f5f487bd6f129f
roel4d <roel4d@webding.org>
2025-07-24 13:03:38 +0200

    tab in lazyvim

--- lazyvim-options.lua
+++ lazyvim-options.lua
@@ -2,10 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-
 -- symlink to ~/.config/nvim/lua/config/options.lua
 
 vim.opt.relativenumber = false
 vim.opt.number = false
 vim.opt.signcolumn = "no"
 vim.opt.list = false
+
+-- change window also with tab
+vim.keymap.set("n", "<tab>", "<c-w>w", { noremap = true })
+vim.keymap.set("n", "<S-tab>", "<c-w>W", { noremap = true })
